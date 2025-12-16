44fc4a7c3f345e48005c2900a222751b3e361518
gzp79 <->
2025-12-16 14:15:43 +0100

    Theme config menu

---
+++ src/lib/theme/ThemeMenu.stories.svelte
@@ -0,0 +1,77 @@
+<script module lang="ts">
+    import { defineMeta } from '@storybook/addon-svelte-csf';
+    import { expect, userEvent, within } from 'storybook/test';
+    import { tick } from 'svelte';
+    import ThemeMenu from '@lib/theme/ThemeMenu.svelte';
+    import { type Theme, themeList, themeStore } from '@lib/theme/theme.svelte';
+    import Dropdown from '@lib/ui/atoms/dropdown-menu';
+
+    const { Story } = defineMeta({
+        title: 'Components/ThemeMenu',
+        component: ThemeMenu,
+        play: async ({ canvasElement }) => {
+            expect(canvasElement).toBeDefined();
+        }
+    });
+</script>
+
+<script lang="ts">
+    const themeRegexp = new RegExp('Theme|Téma', 'i');
+    const regexps: Record<Theme, RegExp> = {
+        system: new RegExp('System|Rendszer', 'i'),
+        dark: new RegExp('Dark|Sötét', 'i'),
+        light: new RegExp('Light|Világos', 'i')
+    };
+
+    let theme = themeStore();
+    const waitForTransition = (ms = 500) => new Promise((resolve) => setTimeout(resolve, ms));
+</script>
+
+<Story
+    name="ThemeSelector"
+    play={async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+        const trigger = await canvas.getByRole('button');
+
+        // Open dropdown
+        await userEvent.click(trigger!, { pointerEventsCheck: 0 });
+        await waitForTransition();
+
+        // Open theme submenu
+        const themeSubTrigger = await canvas.getByRole('menuitem', { name: themeRegexp });
+        await userEvent.hover(themeSubTrigger!);
+        await waitForTransition();
+
+        // Check each theme option
+        for (const option of themeList) {
+            await expect(option).toBeDefined();
+            const optionItem = await canvas.getByRole('menuitemradio', { name: regexps[option] });
+            await userEvent.click(optionItem);
+            await tick();
+            await expect(theme.current).toBe(option);
+        }
+
+        // Close dropdown
+        await userEvent.click(trigger!, { pointerEventsCheck: 0 });
+        await waitForTransition();
+    }}
+>
+    {#snippet template(args)}
+        <Dropdown.Menu>
+            <Dropdown.Trigger>Setting [{theme.current}]</Dropdown.Trigger>
+            <Dropdown.Content class="w-56" align="start">
+                <Dropdown.Group>
+                    <Dropdown.GroupHeading>Settings</Dropdown.GroupHeading>
+                    <Dropdown.Item>Profile</Dropdown.Item>
+                    <Dropdown.Item>Account</Dropdown.Item>
+                </Dropdown.Group>
+                <Dropdown.Separator />
+                <ThemeMenu />
+                <Dropdown.Separator />
+                <Dropdown.Item>Help</Dropdown.Item>
+            </Dropdown.Content>
+        </Dropdown.Menu>
+
+        <div id="popover"></div>
+    {/snippet}
+</Story>
---
+++ src/lib/theme/ThemeMenu.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { t } from '@lib/i18n/i18n.svelte';
+    import { themeList, themeStore } from '@lib/theme/theme.svelte';
+    import Dropdown from '@lib/ui/atoms/dropdown-menu';
+    import DarkIcon from '@lib/ui/atoms/icons/common/Dark.svelte';
+    import DarkLightIcon from '@lib/ui/atoms/icons/common/DarkLight.svelte';
+    import LightIcon from '@lib/ui/atoms/icons/common/Light.svelte';
+
+    let theme = themeStore();
+
+    const themeIcons = {
+        dark: DarkIcon,
+        light: LightIcon,
+        system: DarkLightIcon
+    };
+</script>
+
+<Dropdown.Sub>
+    <Dropdown.SubTrigger>{$t('common.theme')}</Dropdown.SubTrigger>
+    <Dropdown.SubContent>
+        <Dropdown.RadioGroup bind:value={theme.current}>
+            {#each themeList as themeOption (themeOption)}
+                {@const Icon = themeIcons[themeOption]}
+                <Dropdown.RadioItem value={themeOption} closeOnSelect={false}>
+                    <Icon />
+                    {$t(`common.theme.${themeOption}`)}
+                </Dropdown.RadioItem>
+            {/each}
+        </Dropdown.RadioGroup>
+    </Dropdown.SubContent>
+</Dropdown.Sub>
