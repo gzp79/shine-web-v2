02513756748810295167907ffd2535ee77e6a8b1
gzp79 <->
2025-12-16 11:41:36 +0100

    CI pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,18 +5,24 @@ console.log(`Environment: (${config.environment})`);
 if (['dev', 'local', 'mock'].includes(config.environment)) {
     process.env.DEBUG = 'log:user, log:game, warn:*, info:*';
 }
+const isCI = !!process.env.DEPLOYMENT_URL;
+
+const webURL = process.env.DEPLOYMENT_URL || config.webUrl;
+if (isCI && config.environment !== 'prod') {
+    throw new Error('CI deployment shall only use prod environment');
+}
 
 export default defineConfig({
     webServer: {
         command: 'pnpm run build && pnpm run preview',
-        port: parseInt(new URL(config.webUrl).port)
+        port: parseInt(new URL(webURL).port)
     },
     testDir: 'tests/e2e',
     use: {
-        baseURL: config.webUrl,
+        baseURL: webURL,
         headless: true,
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
-        ignoreHTTPSErrors: true
+        ignoreHTTPSErrors: !isCI
     }
 });
