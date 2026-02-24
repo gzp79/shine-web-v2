f83c2f3c2213c824ccf87abcdda0cfba820bffa0
Zoltán Gaál <51709076+gzp79@users.noreply.github.com>
2026-02-24 23:44:23 +0100

    Check if test works in CI
    Added a test to check if a button is rendered with the correct value.

--- src/lib/ui/atoms/input/Button.test.ts
+++ src/lib/ui/atoms/input/Button.test.ts
@@ -13,6 +13,7 @@ describe('Button', () => {
 
         const button = screen.getByRole('button');
         expect(button).toBeInTheDocument();
+        expect(true).toBe("alma");
     });
 
     test('calls onclick handler when clicked', async () => {
